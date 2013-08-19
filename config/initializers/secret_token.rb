21e031f4d2052db0e689f2f543584b88fef34c19
rhengstermann <rene.hengstermann@etecture.de>
2013-08-19 11:25:17 +0200

    Initial project commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentRooms::Application.config.secret_token = '48dd6b541a18a460bdfd24b1d8edd521791cc8f4d989ccdeaf45ff345f967df17090c7ee73eb39c3645d63ba0ed2b28b868fa1b725c0a99af10a8306dd883f7d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StudentRooms::Application.config.session_store :cookie_store, key: '_student-rooms_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StudentRooms::Application.config.session_store :active_record_store
